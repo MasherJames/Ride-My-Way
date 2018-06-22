952c0cc805c22b763abb907728561ff4c88a5740
James Macharia <jamesmasher13@gmail.com>
2018-06-22 03:12:33 +0300

    [Feature #158460445] Add test Loader functionality

--- run.py
+++ run.py
@@ -1,16 +1,11 @@
 import os
 from app import create_app
-from app.models import store, Rides
 
-app = create_app(os.environ.get('MODE') or 'default')
+app = create_app(os.getenv('MODE') or 'default')
 
 
 @app.cli.command()
 def test():
     import unittest
     t = unittest.TestLoader().discover('test')
-    unittest.TextTestRunner(verbosity=3).run(t)
-
-
-# if __name__ == '__main__':
-#     app.run()
+    unittest.TextTestRunner(verbosity=1).run(t)
