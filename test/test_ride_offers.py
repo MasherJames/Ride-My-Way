e268499ae55392a44cf63f4878d3bf399f2c4680
James Macharia <jamesmasher13@gmail.com>
2018-06-23 12:46:02 +0300

    [Fixes #158460445] Fix tests

--- test/test_ride_offers.py
+++ test/test_ride_offers.py
@@ -1,8 +1,6 @@
-# from datetime import datetime
 from app import create_app
 import unittest
 import json
-# from datetime import datetime
 
 
 class TestRideOffers(unittest.TestCase):
@@ -11,13 +9,13 @@ class TestRideOffers(unittest.TestCase):
         self.client = self.app.test_client()
         self.data = {
             "signup-cred": {
-                "fullname": "james mash",
+                "username": "macharia",
                 "email": "jamesmash@gmail.com",
                 "password": "qwerty",
                 "permission": 1
             },
             "login-cred": {
-                "email": "jamesmash@gmail.com",
+                "username": "macharia",
                 "password": "qwerty"
             }
         }
@@ -35,6 +33,7 @@ class TestRideOffers(unittest.TestCase):
             data=json.dumps(self.data['signup-cred']),
             headers={'content-type': 'application/json'}
         )
+
         return response
 
     '''
@@ -45,47 +44,63 @@ class TestRideOffers(unittest.TestCase):
         response = self.client.post(
             "/api/v1/auth/login",
             data=json.dumps(self.data['login-cred']),
-            headers={'content-type': 'application/json'})
+            headers={'content-type': 'application/json'}
+        )
 
         return response
+
+    def test_signup(self):
+        response = self.signup()
+
+        self.assertEqual(response.status_code, 201)
+
+    def test_login(self):
+        ''' signup a user first '''
+        self.signup()
+        response = self.login()
+
+        self.assertEqual(response.status_code, 200)
+
     '''
     get token function
     '''
 
-    # def get_user_token(self):
-    #     self.signup()
-    #     response = self.login()
-    #     token = json.loads(response.data).get('token', None)
+    def get_user_token(self):
+        self.signup()
+        response = self.login()
+        token = json.loads(response.data).get('token', None)
 
-    #     return token
+        return token
 
-    # def test_user_get_token(self):
-    #     self.signup()
-    #     response = self.login()
-    #     self.assertEqual(response.status_code, 200)
+    def test_user_get_token(self):
+        self.signup()
+        response = self.login()
+        self.assertEqual(response.status_code, 200)
 
-    #     self.assertIn('token', json.loads(response.data))
+        self.assertIn('token', json.loads(response.data))
 
     def test_user_can_view_all_ride_offers(self):
-        # token = self.get_user_token()
+        token = self.get_user_token()
 
         response = self.client.get(
             "/api/v1/rides",
-            headers={'content-type': 'application/json'}
+            headers={'content-type': 'application/json',
+                     'Authorization': 'Bearer {}'.format(token)}
         )
         self.assertEqual(response.status_code, 200)
 
     def test_user_can_view_a_specific_ride_offer(self):
-        # token = self.get_user_token()
+        token = self.get_user_token()
 
         response = self.client.get(
-            "/api/v1/rides/<rideId >",
-            headers={'content-type': 'application/json'}
+            "/api/v1/rides/<rideId>",
+            headers={'content-type': 'application/json',
+                     'Authorization': 'Bearer {}'.format(token)}
         )
         self.assertEqual(response.status_code, 200)
 
     def test_user_can_create_ride_offer(self):
-        # token = self.get_user_token()
+        token = self.get_user_token()
 
         data = {
             "driver_name": "joseph",
@@ -95,23 +110,21 @@ class TestRideOffers(unittest.TestCase):
         }
         response = self.client.post(
             "/api/v1/rides", data=json.dumps(data),
-            headers={'content-type': 'application/json'}
+            headers={'content-type': 'application/json',
+                     'Authorization': 'Bearer {}'.format(token)}
         )
 
         self.assertEqual(response.status_code, 201)
 
     def test_user_can_request_a_ride(self):
-        # token = self.get_user_token()
+        token = self.get_user_token()
 
         data = {
-            "name": "james",
-            "from": "meru",
-            "to": "Rongai",
-            "departure": " 12th nov 2018"
         }
 
         response = self.client.post(
             "/api/v1/rides/1/requests", data=json.dumps(data),
-            headers={'content-type': 'application/json'}
+            headers={'content-type': 'application/json',
+                     'Authorization': 'Bearer {}'.format(token)}
         )
         self.assertEqual(response.status_code, 201)
