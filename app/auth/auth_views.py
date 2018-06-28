e38b9b9ad4218ea5c15ecacde09e6c07eec3865a
James Macharia <jamesmasher13@gmail.com>
2018-06-28 03:57:09 +0300

    [Chore #158625590] Add get ride requests resource

--- app/auth/auth_views.py
+++ app/auth/auth_views.py
@@ -4,7 +4,8 @@ from flask_jwt_extended import (
     create_access_token,
     get_jwt_identity, jwt_required
 )
-# from ..models import UserRegister, UserLogin, store
+
+from ..models import UserRegister
 
 
 class Signup(Resource):
@@ -19,17 +20,6 @@ class Signup(Resource):
     parser.add_argument('permission', required=True,
                         help='This field cannot be left blank')
 
-    @classmethod
-    def find_user_by_username(cls, username):
-        for user in store['users']:
-            return user.username == username
-
-    @classmethod
-    def get_user_by_username(cls, username, password):
-        for user in store['users']:
-            if user.username == username and user.check_password(password):
-                return user
-
     def post(self):
         request_data = Signup.parser.parse_args()
 
@@ -38,12 +28,13 @@ class Signup(Resource):
         password = request_data['password']
         permission = request_data['permission']
 
-        if Signup.find_user_by_username(username):
+        user_reg = UserRegister()
+        if user_reg.get_user_by_username(username):
             return {'message': 'user with the username {} already exists'
                     .format(username)}, 400
 
         user = UserRegister(username, email, password, permission)
-        store['users'].append(user)
+        user.add_user()
         return {'message': 'user created successfully'}, 201
 
 
@@ -59,9 +50,9 @@ class Login(Resource):
         request_data = Login.parser.parse_args()
 
         username = request_data['username']
-        password = request_data['password']
 
-        user = Signup.get_user_by_username(username, password)
+        user_reg = UserRegister()
+        user = user_reg.get_user_by_username(username)
 
         if user:
             token = create_access_token(user.username)
