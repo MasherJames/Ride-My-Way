373a467f4232c8051d73b4daa2721453d56cefca
James Macharia <jamesmasher13@gmail.com>
2018-06-28 03:59:42 +0300

    [Chore #158625590] Import postride resource

--- app/api_v1/__init__.py
+++ app/api_v1/__init__.py
@@ -1,4 +1,4 @@
 from flask import Blueprint
-from .views import RideOffers, RideOffer, Request
+from .views import RideOffers, RideOffer, Request, FetchedRideRequest, PostRide
 
 api_bp = Blueprint('api', __name__)
